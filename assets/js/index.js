6a8043f22602a813007726723a5a1c1154e60fcc
hama0000 <hirokih3036@gmail.com>
2020-03-07 21:30:17 +0900

    数字部分実装

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,35 @@
 $(function () {
+
+    var $val = '';
+    var $ope = '';
+    var $result = '';
+
+    $('.num').on('click', function () {
+        $val += $(this).val();
+        $('.textbox').val($val);
+        console.log($val);
+    });
+
+    $('.ope').on('click', function () {
+        $ope += $(this).val();
+        $('.textbox').val($ope);
+
+        /*
+        if ($result == '') {
+            $result += $val;
+        } else {
+            $result = ;
+        }
+        */
+        console.log($result);
+        $val = '';
+        $ope = '';
+    });
+
+    $('.iqual').on('click', function () {
+        $ope += $(this).val();
+        $ope = '';
+    });
+
+
 });
