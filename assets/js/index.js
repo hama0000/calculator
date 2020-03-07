56c3b49645d8e8de4e65cefa73231de244682863
hama0000 <hirokih3036@gmail.com>
2020-03-08 00:47:15 +0900

    ボタンをbuttonタグに変更&レイアウトをtableタグに変更

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,20 @@
 $(function () {
 
     var $val = '';
-    var $ope = '';
     var $result = '';
 
-    $('.num').on('click', function () {
-        $val += $(this).val();
-        $('.textbox').val($val);
+    $('button').on('click', function () {
+        $val += $(this).text();
+        $('#textbox').text($val);
         console.log($val);
     });
 
-    $('.ope').on('click', function () {
-        $ope += $(this).val();
-        $('.textbox').val($ope);
 
-        /*
-        if ($result == '') {
-            $result += $val;
-        } else {
-            $result = ;
-        }
-        */
-        console.log($result);
-        $val = '';
-        $ope = '';
-    });
-
+    /*
     $('.iqual').on('click', function () {
-        $ope += $(this).val();
-        $ope = '';
+
     });
+    */
 
 
 });
