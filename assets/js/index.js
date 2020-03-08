009dd07512a129e11498ae8773d578cee3c3645b
hama0000 <hirokih3036@gmail.com>
2020-03-08 13:05:03 +0900

    連続で符号が入力できないように修正

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,48 @@
 $(function () {
 
-    $('button').on('click', function () {
+    const $button = $('button');
+    var $distinction = 0;
+
+    $button.on('click', function () {
 
         var $textbox = $('#textbox');
-        var $pushed = $(this).text();
+        var $pushed = $(this);
+        var $pushedText = $(this).text();
 
-        if ($pushed == '=') {
+        if ($pushedText == '=') {
             // イコールが押されたときの処理
             $textbox.text(eval($textbox.text()));
 
-        } else if ($pushed == 'AC') {
+        } else if ($pushedText == 'AC') {
             // ACが押されたときの処理
             $textbox.text('0');
 
         } else {
             // その他のボタンが押されたときの処理
             if ($textbox.text() == '0') {
-                $textbox.text($pushed);
+                // 0が押されたとき
+                $textbox.text($pushedText);
 
             } else {
-                $textbox.text($textbox.text() + $pushed);
+                if ($pushed.is('#operator')) {
+                    // 演算子が押されたときの処理
+                    if ($distinction == 0) {
+                        // 直前に演算子が押されていないときの処理
+                        $textbox.text($textbox.text() + $pushedText);
+                        $distinction = 1;
+                        console.log("演算子が押されました");
+
+                    } else {
+                        // 直前に演算子が押されていたときの処理
+                        return;
+                    }
+
+                } else {
+                    // 演算子以外が押されたとき
+                    $textbox.text($textbox.text() + $pushedText);
+                    $distinction = 0;
+                }
+
             }
 
         }
