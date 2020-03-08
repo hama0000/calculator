c30ee286ef3f863436887bb46f54a3437d5426c5
hama0000 <hirokih3036@gmail.com>
2020-03-08 13:20:25 +0900

    表示部分の更新処理をアロー関数式として定義

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,12 +3,20 @@ $(function () {
     const $button = $('button');
     var $distinction = 0;
 
+
     $button.on('click', function () {
 
+        // 関数の定義
         var $textbox = $('#textbox');
         var $pushed = $(this);
         var $pushedText = $(this).text();
 
+        const textChange = () => {
+            $textbox.text($textbox.text() + $pushedText);
+        }
+
+
+        // ボタンの処理
         if ($pushedText == '=') {
             // イコールが押されたときの処理
             if ($distinction == 0) {
@@ -30,19 +38,19 @@ $(function () {
                     // 演算子が押されたときの処理
                     if ($distinction == 0) {
                         // 直前に演算子が押されていないときの処理
-                        $textbox.text($textbox.text() + $pushedText);
+                        textChange();
+                        //$textbox.text($textbox.text() + $pushedText);
                         $distinction = 1;
                         console.log("演算子が押されました");
 
                     } // 直前に演算子が押されていたときの処理はなし
 
-
                 } else {
                     // 演算子以外が押されたとき
-                    $textbox.text($textbox.text() + $pushedText);
+                    textChange();
+                    //$textbox.text($textbox.text() + $pushedText);
                     $distinction = 0;
                 }
-
             }
 
         }
